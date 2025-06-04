c401f501f35bf5b7d2bbdda5c8ff80ee152356c2
Mateomelano <mateomelano2003@gmail.com>
2025-06-04 16:11:07 -0300

    cambio de base de datos para prod

--- src/php/db.php
+++ src/php/db.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "185.211.7.154"; // o el host específico que te da Hostinger (ej: `srv123.main-hosting.eu`)
-$user = "u617835785_root"; // tu usuario exacto
+$user = "u617835785_root1"; // tu usuario exacto
 $password = "Merceriachela1"; // tu contraseña real
-$dbname = "u617835785_mc_aromas";
+$dbname = "u617835785_merceria";
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
